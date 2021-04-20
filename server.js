26664719a71bd2ad0b1d584b67ea54128123c431
OrionStark <robbybellamy6@gmail.com>
2021-04-20 16:51:37 +0700

    Update stratum server

---
+++ server.js
@@ -0,0 +1,94 @@
+const stratum = require('./lib/index');
+const btc = {
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    algorithm: 'sha256',
+    nValue: 1024,
+    rValue: 1,
+    txMessages: false
+};
+
+const pool = stratum.createPool({
+    coin: btc,
+    address: 'bc1q9ftj4dp220srmj45jujwj7et38nr5lkukw9ylk',
+    rewardRecipients: {
+        "n37vuNFkXfk15uFnGoVyHZ6PYQxppD3QqK": 1.5,
+        "mirj3LtZxbSTharhtXvotqtJXUY7ki5qfx": 0.5,
+        "22851477d63a085dbc2398c8430af1c09e7343f6": 0.1
+    },
+    blockRefreshInterval: 1000,
+    jobRebroadcastTimeout: 55,
+    connectionTimeout: 600,
+    emitInvalidBlockHashes: false,
+    tcpProxyProtocol: false,
+    banning: {
+        enabled: true,
+        time: 600,
+        invalidPercent: 50,
+        checkThreshold: 500,
+        purgeInterval: 300
+    },
+    ports: {
+        '3032': {
+            diff: 32,
+            varDiff: {
+                minDiff: 8,
+                maxDiff: 512,
+                targetTime: 15,
+                retargetTime: 90,
+                variancePercent: 30
+            }
+        },
+        '3256': {
+            diff: 256
+        }
+    },
+    daemons: {
+        host: "127.0.0.1",
+        port: 8332,
+        user: "bitcoin",
+        password: "testing"
+    },
+    // p2p: {
+    //     enabled: false,
+    //
+    //     /* Host for daemon */
+    //     host: "127.0.0.1",
+    //
+    //     /* Port configured for daemon (this is the actual peer port not RPC port) */
+    //     port: 19333,
+    //
+    //     /* If your coin daemon is new enough (i.e. not a shitcoin) then it will support a p2p
+    //        feature that prevents the daemon from spamming our peer node with unnecessary
+    //        transaction data. Assume its supported but if you have problems try disabling it. */
+    //     disableTransactions: true
+    //
+    // }
+}, (ip, port , workerName, password, callback) => {
+    console.log("Authorize " + workerName + ":" + password + "@" + ip);
+    callback({
+        error: null,
+        authorized: true,
+        disconnect: false
+    });
+});
+
+pool.on('share', function(isValidShare, isValidBlock, data){
+
+    if (isValidBlock)
+        console.log('Block found');
+    else if (isValidShare)
+        console.log('Valid share submitted');
+    else if (data.blockHash)
+        console.log('We thought a block was found but it was rejected by the daemon');
+    else
+        console.log('Invalid share submitted')
+
+    console.log('share data: ' + JSON.stringify(data));
+});
+
+pool.on('log', function(severity, logKey, logText){
+    console.log(severity + ': ' + '[' + logKey + '] ' + logText);
+});
+
+pool.start();
